28349363c03d1eb93e1658e7a6c8c8bb0cf3e870
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-11-15 17:14:10 +0500

    add red queen and backgrounds

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello World!
+    <div className="wrapper">
+      <div className="sky">
+        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"}/>
+        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"}/>
+      </div>
+      <div className="earth">
+        
+      </div>
+      <img id="red-quen" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"}/>
     </div>
   );
 }
