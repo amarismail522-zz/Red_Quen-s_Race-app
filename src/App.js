1409a940b421f1f664035e31d4ca5a8809812b9a
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-11-16 23:40:40 +0500

    update app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
+
 
 function App() {
+  const { ref } = useWebAnimations({
+    keyframes: {
+      transform: ['translateY(0)',],
+      transform: ['translateY(100%)']
+    },
+    timing: {
+      easing: 'steps(7, end)',
+      direction: "reverse",
+      duration: 5000,
+      playbackRate: 1,
+      iterations: Infinity
+    }
+  });
   return (
     <div className="wrapper">
       <div className="sky">
-        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"}/>
-        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"}/>
+        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"} />
+        <img id="bg-sky" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"} />
       </div>
       <div className="earth">
-        
       </div>
-      <img id="red-quen" src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"}/>
+      <div id="red-quen-div">
+      <img id="red-quen" ref={ref} src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"} />
+
+      </div>
     </div>
   );
 }
