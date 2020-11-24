32329cd9d4f93a1de14abdeccbed1a0ee6de7edc
amarismail522 <56680798+amarismail522@users.noreply.github.com>
2020-11-24 08:31:21 +0500

    update app.css and app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
-import useWebAnimations from "@wellyshen/use-web-animations";
-
 
 function App() {
-  
+  const redQueen = useRef(null);
+
+
+  useEffect(() => {
+    var queenFrames = [
+      { transform: "translateY(0)" },
+      { transform: "translateY(-100%)" }
+    ];
+
+    var Queen = redQueen.current.animate(
+      queenFrames, {
+      easing: 'steps(7, end)',
+      direction: "reverse",
+      duration: 1000,
+      playbackRate: 1,
+      iterations: Infinity
+    });
+  })
+
+
   return (
-   <div className="wrapper">
-     <div className="sky">
-       <img src={"https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bg_sky.jpg"} alt=" "/>
-     </div>
-   </div>
+    <div className="wrapper">
+      <div className="sky"></div>
+      <div className="earth">
+        <div className="aliceQueen">
+          <img className="aliceQueen-img" ref={redQueen} alt=" " src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" />
+        </div>
+      </div>
+
+    </div>
   );
 }
 
